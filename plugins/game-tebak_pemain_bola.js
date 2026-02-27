de6b43a85323f84b5dab2d5a55b8b2eb59476d4d
Raya2399 <102332034+Raya2399@users.noreply.github.com>
2026-02-27 18:52:17 +0800

    Change game from tebakbola to tebakpresiden

--- plugins/game-tebak_pemain_bola.js
+++ plugins/game-tebak_pemain_bola.js
@@ -3,14 +3,14 @@ let fetch = require('node-fetch')
 let timeout = 100000
 let poin = 10000
 let handler = async (m, { conn, usedPrefix }) => {
-    conn.tebakbola = conn.tebakbola ? conn.tebakbola : {}
+    conn.tebakpresiden = conn.tebakpresiden ? conn.tebakpresiden : {}
     let id = m.chat
-    if (id in conn.tebakbola) {
-        conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.tebakbola[id][0])
+    if (id in conn.tebakpresiden) {
+        conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.tebakpresiden[id][0])
         throw false
     }
     // di sini dia ngambil data dari api
-    let src = await (await fetch(`https://api.botcahx.eu.org/api/game/tebakpemainbola?apikey=${btc}`)).json()
+    let src = await (await fetch(`https://api.botcahx.eu.org/api/game/tebakpresiden?apikey=${btc}`)).json()
     let json = src
     // buat caption buat di tampilin di wa
     let caption = `
@@ -18,23 +18,23 @@ ${json.soal}
 
 ┌─⊷ *SOAL*
 ▢ Timeout *${(timeout / 1000).toFixed(2)} detik*
-▢ Ketik ${usedPrefix}tboa untuk bantuan
+▢ Ketik ${usedPrefix}pra untuk bantuan
 ▢ Bonus: ${poin} money
 ▢ *Balas/ reply soal ini untuk menjawab*
 └──────────────
 `.trim()
-    conn.tebakbola[id] = [
+    conn.tebakpresiden[id] = [
         await conn.reply(m.chat, caption, m),
         json, poin,
         setTimeout(() => {
-            if (conn.tebakbola[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, conn.tebakbola[id][0])
-            delete conn.tebakbola[id]
+            if (conn.tebakpresiden[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, conn.tebakpresiden[id][0])
+            delete conn.tebakpresiden[id]
         }, timeout)
     ]
 }
-handler.help = ['tebakbola']
+handler.help = ['tebakpresiden']
 handler.tags = ['game']
-handler.command = /^tebakbola/i
+handler.command = /^tebakpresiden/i
 handler.register = false
 handler.group = true
 
